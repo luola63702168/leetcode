abc12dff67aa3541cf59a0cd099e26b1090cb9ea
“luola” <ll63702168@qq.com>
2020-03-20 16:36:32 +0800

    bnar

--- demo.py
+++ demo.py
@@ -4,21 +4,27 @@
 # [1 2 3 -1 5]
 # input: a[]
 # output: True/False
+
+
 def sort_card(nums):
-    leg = len(nums)
     nums.sort()
-    tmp = 0
-    for i in range(leg):
-        if nums[i] != -1:
-            tmp = i
-            break
+
+    # leg = len(nums)
+    # tmp = 0
+    # for i in range(leg):
+    #     if nums[i] != -1:
+    #         tmp = i
+    #         break
+
+    tmp = nums.count(-1)
     tmp_nums = nums[tmp:]
-    while tmp > 0:
+    while tmp >= 0:
         for p in range(1, len(tmp_nums)):
             if tmp_nums[p] - tmp_nums[p-1] != 1:
                 tmp_nums.insert(p-1, tmp_nums[p-1] + 1)
                 tmp -= 1
                 break
+        tmp_nums.sort()
         tmp -= 1
     print(tmp_nums)
 
@@ -26,4 +32,4 @@ def sort_card(nums):
 
 
 if __name__ == '__main__':
-    sort_card([-1, -1, 3, 4, 5])
+    sort_card([-1, -1, 3, 8, 5])
