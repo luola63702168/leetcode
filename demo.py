4d854b7113b3d8de6f533e808f2dcc62d46a747c
“luola” <ll63702168@qq.com>
2020-03-25 20:56:15 +0800

    up

--- demo.py
+++ demo.py
@@ -8,26 +8,17 @@
 
 def sort_card(nums):
     nums.sort()
-
-    # leg = len(nums)
-    # tmp = 0
-    # for i in range(leg):
-    #     if nums[i] != -1:
-    #         tmp = i
-    #         break
-
     tmp = nums.count(-1)
     tmp_nums = nums[tmp:]
     while tmp >= 0:
         for p in range(1, len(tmp_nums)):
             if tmp_nums[p] - tmp_nums[p-1] != 1:
-                tmp_nums.insert(p-1, tmp_nums[p-1] + 1)
+                tmp_nums.insert(p, tmp_nums[p-1] + 1)
                 tmp -= 1
+            if tmp == -1:
                 break
-        tmp_nums.sort()
         tmp -= 1
     print(tmp_nums)
-
     return
 
 
